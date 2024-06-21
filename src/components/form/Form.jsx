cd9591de2fd4819b9716843b73087d80b2f1ed95
julien henry <julienhenry967@gmail.com>
2024-06-21 16:38:09 +0200

    chore: update fields

--- src/components/form/Form.jsx
+++ src/components/form/Form.jsx
@@ -1,36 +1,34 @@
 import Button from "../button/Button";
 import TextField from "../textField/TextField";
+import Select from "../select/Select";
+import { departments, states } from "../../utils/utils";
 import "./Form.css";
 import "../textField/TextField.css";
-import Select from "../select/Select";
 
 const Form = () => {
+  const statesName = states.map((state) => state.name);
   const handleClick = (e) => {
     e.preventDefault();
     console.log("Hello");
   };
   return (
     <form className="form">
-      <TextField type="text" id="firstName" label="First Name" />
-      <TextField type="text" id="lastName" label="Last Name" />
+      <TextField type="text" id="first-name" label="First Name" />
+      <TextField type="text" id="last-name" label="Last Name" />
 
       {/* DATE PICKER */}
-      <TextField type="text" id="birthDate" label="Birth Date" />
-      <TextField type="text" id="startDate" label="Start Date" />
+      <TextField type="text" id="date-of-birth" label="Birth Date" />
+      <TextField type="text" id="start-date" label="Start Date" />
       {/* ////// */}
 
       <TextField type="text" id="street" label="Street" />
       <TextField type="text" id="city" label="City" />
 
-      <Select id="state" label="State" options={["Alabama", "Alaska"]} />
+      <Select id="state" label="State" options={statesName} />
 
-      <TextField type="number" id="zipCode" label="zipCode" />
+      <TextField type="number" id="zip-code" label="Zip code" />
 
-      <Select
-        id="department"
-        label="Department"
-        options={["Alabama", "Alaska"]}
-      />
+      <Select id="department" label="Department" options={departments} />
 
       <Button handleClick={handleClick}>Save</Button>
     </form>
