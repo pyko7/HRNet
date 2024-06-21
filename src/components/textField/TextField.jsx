037f62225c9db3cca007163ba54ac33e839bb546
julien henry <julienhenry967@gmail.com>
2024-06-21 14:34:07 +0200

    chore: remove className props

--- src/components/textField/TextField.jsx
+++ src/components/textField/TextField.jsx
@@ -4,14 +4,13 @@ import "./TextField.css";
  * @param {string} id Input's id
  * @param {string} type Input's type
  * @param {string} label Input's label
- * @param {string} className Input's className
  * @returns
  */
-const TextField = ({ id, type, label, className }) => {
+const TextField = ({ id, type, label }) => {
   return (
     <div>
       {label && <label htmlFor={id}>{label}</label>}
-      <input type={type} id={id} className={`textfield ${className}`} />
+      <input type={type} id={id} className={"textfield"} />
     </div>
   );
 };
